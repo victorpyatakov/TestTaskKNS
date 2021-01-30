ef5221b0f67b2eadd3789fb72b9a486629ecc336
VPyatakov <victor.pyatakov95@yandex.ru>
2021-01-30 05:00:53 +0300

    add comments

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class BracketAnalyze():
     Класс для анализа количества открывающих и закрывающих скобок в строке
     """
     def __init__(self, string_with_brackets:str):
-        self.string_with_brackets = string_with_brackets
+        self.__string_with_brackets = string_with_brackets
 
 
     def __get_dict_brakets(self) -> dict:
@@ -28,7 +28,7 @@ class BracketAnalyze():
         """
         dict_brackets = self.__get_dict_brakets()
         list_brackets = []
-        for char in self.string_with_brackets:
+        for char in self.__string_with_brackets:
             if char in dict_brackets.values():
                     list_brackets.append(char)
                     continue
@@ -46,7 +46,7 @@ class BracketAnalyze():
 
 
 if __name__ == "__main__":
-    test_string = '((({([{}])})))'
+    test_string = '((({([{[ }])})))'
     brackets_analiser = BracketAnalyze(test_string)
     result = brackets_analiser.is_count_brackets_equal()
     print(result)
