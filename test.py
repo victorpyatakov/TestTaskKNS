29dab2f39c579ddc10c83e45166c56f5fa0d79c8
VPyatakov <victor.pyatakov95@yandex.ru>
2021-01-30 04:49:42 +0300

    refactoring

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+"""
+Тестовое задание ООО КНС ГРУПП
+Пятаков В. С.
+30.01.2021
+"""
 class BracketAnalyze():
     """
     Класс для анализа количества открывающих и закрывающих скобок в строке
     """
     def __init__(self, string_with_brackets:str):
         self.string_with_brackets = string_with_brackets
+
 
     def __get_dict_brakets(self) -> dict:
         """
@@ -14,6 +20,7 @@ class BracketAnalyze():
                     ']' : '[',
                     '}' : '{'
         }           
+
 
     def is_count_brackets_equal(self) -> bool:
         """
@@ -36,34 +43,7 @@ class BracketAnalyze():
         else:
             return True
 
-"""
-def brackets_cointer(string):
-    dict_brackets = {
-                        ')' : '(',
-                        ']' : '[',
-                        '}' : '{'
-    }
-    list_brackets = []
-    for char in string:
-        if char in dict_brackets.values():
-                list_brackets.append(char)
-                continue
-        if char in dict_brackets:
-            if dict_brackets[char] in list_brackets:
-                list_brackets.pop()
-                continue
-            else:
-                return False
-    if len(list_brackets) > 0:
-        return False
-    else:
-        return True
 
-
-test_str = '(({([])}))'
-res =   brackets_cointer(test_str)
-print(res)
-"""
 
 if __name__ == "__main__":
     test_string = '((({([{}])})))'
